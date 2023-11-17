d24f172b0e98e27e76b1533064bbec4038cf237d
Tien192 <tranxuantien192@gmail.com>
2023-11-17 23:13:06 +0700

    build source

---
+++ README.md
@@ -0,0 +1,2 @@
+# Checkers
+Checkers game with two modes for playing, 2-players or player Vs. computer with Alpha-Beta AI.
---
+++ scripts/game.js
@@ -0,0 +1,244 @@
+
+// window.setInterval(function () {
+//     myGame.AIturn();
+// }, 1000);
+//
+
+class Game {
+    constructor() {
+        this.turn = 0;     // 1: palyer1, 2: palyer2
+        this.AI = false; //by default two players
+        this.selectedChecker = -1;  // for the id of the selected checker
+        this.tileUnderSelectedChecker = -1;
+        this.selectedCheckerLocation = [-1, -1];
+        this.selectedTile = -1;
+        this.selectedTileLocation = [-1, -1];
+        this.winner = 0;
+        this.playerMove;
+        this.thinking = false;
+        this.isDoubleMode = false;
+
+        
+        this.player1Time = 120;
+        this.player2Time = 120;
+        this.timerInterval;
+    }
+
+
+    startTimer() {
+        this.timerInterval = setInterval(() => {
+            if (this.turn === 1) {
+                this.player1Time--;
+                document.getElementById("redtimer").innerHTML = this.formatTime(this.player1Time);
+            } else if (this.turn === 2) {
+                this.player2Time--;
+                document.getElementById("bluetimer").innerHTML = this.formatTime(this.player2Time);
+            }
+            if (this.player1Time <= 0) {
+                this.endGame(2); // Player 2 wins when player 1's time runs out
+            } else if (this.player2Time <= 0) {
+                this.endGame(1); // Player 1 wins when player 2's time runs out
+            }
+        }, 1000); // Update every 1 second
+    }
+
+    formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    }
+
+    moveCheckerP1(locC, locT) {
+        if (!this.AI) {
+            
+            console.log(this.turn);
+            //currBoard.board[locC[0]][locC[1]] = 0;
+            //currBoard.board[locT[0]][locT[1]] = 1;
+            let temMvs = findMovesAI(currBoard.board, currBoard.kingsList, 1);
+            let tem = findMovePlayer(locC, locT, temMvs);
+            makeMoveAI(currBoard.board, currBoard.kingsList, tem, 1);
+            this.next(tem);
+
+            if (noMoreMoves(currBoard.board, currBoard.kingsList, 2)) {
+                this.winner = 1;
+                document.getElementById("winner").innerHTML = "Red won!";
+                this.display_winner();
+                return;
+            }
+
+        }
+        this.resetTimer();
+    };
+
+    moveCheckerP2(locC, locT) {
+
+        console.log(this.turn);
+        //currBoard.board[locC[0]][locC[1]] = 0;
+        //currBoard.board[locT[0]][locT[1]] = 2;
+
+        let temMvs = findMovesAI(currBoard.board, currBoard.kingsList, 2);
+        let tem = findMovePlayer(locC, locT, temMvs);
+        makeMoveAI(currBoard.board, currBoard.kingsList, tem, 2);
+        this.next(tem);
+        
+        if (noMoreMoves(currBoard.board, currBoard.kingsList, 1)) {
+            this.winner = 2;
+            document.getElementById("winner").innerHTML = "Blue won!";
+            this.display_winner();
+            return;
+        }
+        this.resetTimer();
+    };
+
+    resetTimer() {
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+        this.startTimer();
+    }
+    endGame(winner) {
+        this.winner = winner;
+        document.getElementById("winner").innerHTML = winner === 1 ? "Red" : "Blue" + " won on time!";
+        this.display_winner();
+        clearInterval(this.timerInterval);
+    }
+
+    validDiagonalP1(locC, locT) { // red - moves down
+        let temMvs = findMovesAI(currBoard.board, currBoard.kingsList, 1);
+        return isValidMove(locC, locT, temMvs);
+    };
+
+    validDiagonalP2(locC, locT) {   // blue - moves up
+        let temMvs = findMovesAI(currBoard.board, currBoard.kingsList, 2);
+        console.log("moooves");
+        console.log(temMvs);
+        return isValidMove(locC, locT, temMvs);
+    };
+
+
+    //added this cause i noticed some lag when moveAI is put in checker turn , night change it later
+    
+    next(lastMove) {
+
+        currBoard.reDrawBoard();
+
+        let x = this.tileUnderSelectedChecker.toString();
+        x = x.replace("tile", "");
+        document.getElementById("tile" + x).style.boxShadow = "inset 0px 0px 15px 5px rgba(110, 188, 224, 0.75)";
+       
+        x = this.selectedTile.toString();
+        x = x.replace("tile","");
+        document.getElementById("tile" + x).style.boxShadow = "inset 0px 0px 15px 5px rgba(110, 188, 224, 0.75)";
+
+        if (this.isDoubleJump() && lastMove.jump) {
+            this.turn = (this.turn == 1) ? 1 : 2;
+            // selected checker must remains the same.
+            this.selectedCheckerLocation = this.selectedTileLocation;
+            this.isDoubleMode = true;
+        } else {
+            this.turn = (this.turn == 1) ? 2 : 1;
+            this.selectedCheckerLocation = [-1, -1];
+            this.isDoubleMode = false;
+        }
+        this.selectedChecker = -1;  // appearantly the id changes with each render...
+        this.tileUnderSelectedChecker = -1;
+        this.selectedTileLocation = [-1, -1];
+        this.selectedTile = -1;
+        // currBoard.reDrawBoard();
+        console.log("next turn " + this.turn);
+        this.thinking = false;
+        this.update_count_display();
+        if(this.turn == 1){
+            document.getElementById("turn").innerHTML = "RED turn";
+        }
+        else{
+            document.getElementById("turn").innerHTML = "BLUE turn";
+        }
+    }
+
+    onePlayer() {
+        this.AI = true;
+        this.turn = 2;
+        this.display();
+        
+    }
+    twoPlayers() {
+        this.AI = false;
+        this.turn = 2;
+        this.display();
+    }
+    display (){
+        document.getElementById("two").style.display = "none";
+        document.getElementById("one").style.display = "none";
+        document.getElementById("start").innerHTML = "Good Luck!";
+        document.getElementById("red").style.display = "block";
+        document.getElementById("blue").style.display = "block";
+        document.getElementById("turn").style.display = "block";
+        document.getElementById("turn").innerHTML = "BLUE turn"; 
+    }
+    display_winner(){
+        console.log("game ended");
+        this.turn = 0;
+        document.getElementById("winner").style.display = "block";
+        document.getElementById("winneri").style.display = "block";
+        document.getElementById("start").style.display = "none";
+
+        clearInterval();
+    }
+
+    update_count_display (){
+        
+        document.getElementById("redcount").innerHTML = noTiles(currBoard.board, 1);
+        document.getElementById("bluecount").innerHTML = noTiles(currBoard.board, 2);
+
+    }
+
+    saveChecker(id, loc) {
+        this.selectedChecker = id;
+        this.selectedCheckerLocation = loc;
+        // document.getElementById(id).style = ""
+    }
+
+    saveTile(id, loc) {
+        this.selectedTile = id;
+        this.selectedTileLocation = loc;
+    }
+
+    isDoubleJump() {
+        let temMvs = findMovesAI(currBoard.board, currBoard.kingsList, this.turn);
+        if (temMvs.length != 0) {
+            if ((temMvs[0].jump)) {   // if the first one is jump, then all the list is possible jumps for the player
+                for (let i = 0; i < temMvs.length; i++) {   // just check if the jumps are double or not ...
+
+                    if ((temMvs[i].pastlocation[0] == this.selectedTileLocation[0]) && (temMvs[i].pastlocation[1] == this.selectedTileLocation[1])) {
+                        console.log("it's double, yeah!");
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    removeAllGlow() {
+        for (let i = 0; i < 64; i++) {
+            document.getElementById("tile" + i).style.boxShadow = "";
+        }
+    }
+}
+
+// function whatever() {
+//     console.log("hi");
+//     currBoard.board=[
+//             [0, 1, 0, 1, 0, 1, 0, 1],
+//             [0, 0, 1, 0, 1, 0, 1, 0],
+//             [0, 1, 0, 1, 0, 1, 0, 1],
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [0, 0, 0, 0, 0, 0, 0, 0],
+//             [2, 0, 2, 0, 2, 0, 2, 0],
+//             [0, 2, 0, 2, 0, 2, 0, 2],
+//             [2, 0, 2, 0, 2, 0, 2, 0]];
+
+//     currBoard.reDrawBoard();
+// };
+
